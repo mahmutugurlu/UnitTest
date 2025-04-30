406b9487e804ab41ee8cfa72899aba174edc4583
Mahmut Ugurlu <mahmutugurlu0718@gmail.com>
2025-04-30 22:21:50 +0200

    day2

--- src/test/java/test1/Test05RepeatedTest.java
+++ src/test/java/test1/Test05RepeatedTest.java
@@ -2,6 +2,8 @@ package test1;
 
 import org.junit.jupiter.api.RepeatedTest;
 
+import java.util.Random;
+
 import static org.junit.Assert.assertEquals;
 
 public class Test05RepeatedTest {
@@ -20,6 +22,18 @@ public class Test05RepeatedTest {
 
     //repeated çoğunlukla random değerler
     //kullanılan metodların testinde kullanılır
+
+    @RepeatedTest(5)
+    void testAddExactByRandomValue(){
+        Random random=new Random();
+        int sayi1=random.nextInt(100);
+        int sayi2=random.nextInt(100);
+
+        assertEquals(sayi1+sayi2,Math.addExact(sayi1,sayi2));
+
+        System.out.println("sayı1: "+sayi1+"----"+"sayı2: "+sayi2);
+
+    }
 
 
 
---
+++ src/test/java/test1/Test06PerformanceTest.java
@@ -0,0 +1,26 @@
+package test1;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+public class Test06PerformanceTest {
+
+    @Test
+    void testPerformanceSumOperation() {
+
+        assertTimeout(Duration.ofMillis(6),
+                () -> {
+                    long sum = 0;
+                    for (long i = 1; i < 10000000; i++) {
+                        sum += i;
+                    }
+
+                });
+
+
+    }
+}
+
